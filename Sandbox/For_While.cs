a55aa2f030491177de3d902951f3500efe052fc2
Jeff Crowley <jbcrowley@hotmail.com>
2023-06-24 15:56:59 -0500

    For vs While contains test

---
+++ Sandbox/For_While.cs
@@ -0,0 +1,47 @@
+﻿using NUnit.Framework;
+using System;
+using System.Linq;
+
+namespace Sandbox
+{
+    [TestFixture]
+    public class ForWhile
+    {
+        public string actualOwner;
+        public string[] expectedOwners;
+
+        [SetUp]
+        public void Setup()
+        {
+            actualOwner = "C";
+            expectedOwners = new string[] { "A", "B", actualOwner };
+        }
+
+        [Test]
+        [Category("ForWhile")]
+        public void ForTest()
+        {
+            for (int i = 0; !expectedOwners.Contains(actualOwner) && i < 5; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            CollectionAssert.Contains(expectedOwners, actualOwner);
+        }
+
+
+        [Test]
+        [Category("ForWhile")]
+        public void WhileTest()
+        {
+            int i = 0;
+            while (!expectedOwners.Contains(actualOwner) && i < 5)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+
+            CollectionAssert.Contains(expectedOwners, actualOwner);
+        }
+    }
+}
